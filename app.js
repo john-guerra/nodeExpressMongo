9cb005cb2b6419043f847f7b81148803c9af4057
John Alexis Guerra Gómez <john.guerra@gmail.com>
2019-09-02 13:11:58 -0500

    Reading file

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const fs = require("fs");
 
 
+fs.readFile("gananLosCorruptos.json",
+  (err, data) => {
+    if (err) throw err;
+
+    const dataParsed = JSON.parse(data);
+    console.log("got data", dataParsed.length);
+  }
+);
+
+
+
+
 
 
 let a = 1 + 2;
