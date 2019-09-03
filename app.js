cdd826a363c036e59b3ad1a9bbdb9d14e5c7496c
John Alexis Guerra Gómez <john.guerra@gmail.com>
2019-09-03 13:22:24 -0500

    With mongo

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
 const fs = require("fs");
 const express = require("express");
 
+const MongoClient = require("mongodb").MongoClient;
+
 const app = express();
 const PORT = 8080;
 
+const url = "mongodb://localhost:27017";
 
-function readData(fnCBK) {
+const client = new MongoClient(url);
+
+client.connect((err) => {
+  if (err) throw err;
+
+  console.log("Conectado con monguito!!!");
+
+  const db = client.db("nodeExpress3");
+  const colTweets = db.collection("tweets");
+
+  colTweets.find({})
+    .limit(20)
+    .toArray((err, tweets) => {
+      if (err) throw err;
+
+      console.log("# tweets ", tweets.length);
+
+      client.close();
+    });
+
+
+
+});
+
+
+function readData(fnCBK, errCBK) {
   fs.readFile(
     "gananLosCorruptos.json",
     (err, data) => {
-      if (err) throw err;
+      if (err) {
+        errCBK(err);
+        return;
+      }
 
       console.log("dos");
       const dataParsed = JSON.parse(data);
@@ -28,7 +59,11 @@ app.get("/", (req, res) => {
     res.send(data);
   }
 
-  readData(fnCallbackData);
+  function errCBK(err) {
+    res.send(err);
+  }
+
+  readData(fnCallbackData, errCBK);
 
 });
 
